5e6fd2f5d179e7bf5a9ee76eea9aac31898f7bfb
Ben Chen <btchen@utexas.edu>
2019-11-22 20:55:26 -0600

    fixed launch script

--- launch.sh
+++ launch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-ps auxw | grep standalone.py | grep -v grap > /dev/null
+ps auxw | grep standalone.py | grep -v grep > /dev/null 2>&1
 
 if [ $? != 0 ]
 then 
-	python3.6 standalone.py > output.txt
+	python3.6 standalone.py & > /dev/null
 fi
