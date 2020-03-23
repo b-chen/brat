5d288f0e89323b31464f81b0494ec2e3c453c987
Ben Chen <btchen@utexas.edu>
2020-03-23 16:17:02 -0500

    fixed launch script hopefully

--- launch.sh
+++ launch.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
+source $HOME/.bash_profile
+cd /home/ben/brat/
+CWD="$(pwd)"
+echo $CWD
+
 
 ps auxw | grep standalone.py | grep -v grep > /dev/null 2>&1
 
 if [ $? != 0 ]
 then 
-	python3.6 standalone.py & > /dev/null
+	/usr/bin/python3.6 standalone.py &
+	echo launching server
+else
+	echo server is up
 fi
